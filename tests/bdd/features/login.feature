0cfa4de6d49117b9bab85af8d1ad1da0ecbf68d6
anandveeraswamy <anandveeraswamy@gmail.com>
2025-11-18 18:19:40 +0000

    added a simplest feature and step file but with errors to be fixed

---
+++ tests/bdd/features/login.feature
@@ -0,0 +1,10 @@
+Feature: User Login
+  As a registered user
+  I want to log in
+  so I can access my tasks
+
+
+  Scenario: Successful login with correct credentials
+    Given I am on the login page
+    When I enter valid credentials
+    Then I should be redirected to the homepage
---
+++ tests/bdd/steps/test_login_feature.py
@@ -0,0 +1,26 @@
+from pytest_bdd import scenarios, scenario, given, when, then
+from app.models import User
+from app import db
+
+scenarios('../features/login.feature')
+
+
+@given('I am on the login page')
+def on_login_page(client):
+    resp = client.get("/login")
+    assert resp.status_code == 200
+
+@when('I enter valid credentials')
+def enter_valid_credentials(client, auth):
+    resp = client.post(
+        '/login',
+        data= {"username": "testuser", "password": "Password123!"},
+        follow_redirects = True
+    )
+    return resp
+
+@then('I should be redirected to the homepage')
+def redirected_homepage(client, enter_valid_credentials):
+    resp = enter_valid_credentials
+    assert resp.status_code == 200
+    assert b'Hello Testuser' in resp.data
